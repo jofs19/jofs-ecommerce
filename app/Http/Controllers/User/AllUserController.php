29fa507d0411d128df3a17f4fe572b4899d864f3
John <johnoliversantiago5@gmail.com>
2022-08-05 13:02:01 +0800

    changes before section 48

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -54,5 +54,38 @@ class AllUserController extends Controller
      } // end method 
 
 
+     public function ReturnOrder(Request $request,$order_id){
+
+        Order::findOrFail($order_id)->update([
+            'return_date' => Carbon::now()->format('d F Y'),
+            'return_reason' => $request->return_reason,
+        ]);
+
+
+      $notification = array(
+            'message' => 'Return Request Send Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('my.orders')->with($notification);
+
+    } // end method 
+
+    public function ReturnOrderList(){
+
+        $orders = Order::where('user_id',Auth::id())->where('return_reason','!=',NULL)->orderBy('id','DESC')->get();
+        return view('frontend.user.order.return_order_view',compact('orders'));
+
+    } // end method 
+
+
+    public function CancelOrders(){
+
+        $orders = Order::where('user_id',Auth::id())->where('status','cancel')->orderBy('id','DESC')->get();
+        return view('frontend.user.order.cancel_order_view',compact('orders'));
+
+    } // end method 
+
+
 
 }
